d703aa13fcb338c2cb051c989d8cb3185e01fe57
Eric London <eric.london@arcadiasolutions.com>
2017-08-14 08:13:16 -0400

    documentation links

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,4 +1,6 @@
 terraform {
+  # Stores the Terraform state in S3
+  # https://www.terraform.io/docs/backends/types/s3.html
   backend "s3" {
     bucket  = ""
     key     = ""
@@ -7,11 +9,18 @@ terraform {
   }
 }
 
-/*
-data "aws_caller_identity" "current" {
+# AWS ECS cluster
+# TF: https://www.terraform.io/docs/providers/aws/r/ecs_cluster.html
+# AWS: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ECS_clusters.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/ecs/create-cluster.html
+resource "aws_ecs_cluster" "eric_express" {
+  name = "eric-express"
 }
-*/
 
+# [Data] IAM policy document (to allow ECS tasks to assume a role)
+# TF: https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
+# AWS: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/iam/create-policy.html
 data "aws_iam_policy_document" "ecs_assume_role_policy" {
   statement {
     sid = ""
@@ -26,24 +35,36 @@ data "aws_iam_policy_document" "ecs_assume_role_policy" {
   }
 }
 
-resource "aws_ecs_cluster" "eric_express" {
-  name = "eric-express"
-}
-
+# AWS IAM role (to allow ECS tasks to assume a role)
+# TF: https://www.terraform.io/docs/providers/aws/r/iam_role.html
+# AWS: http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/iam/create-role.html
 resource "aws_iam_role" "ecs_role" {
   name               = "eric-ecs-role"
   assume_role_policy = "${data.aws_iam_policy_document.ecs_assume_role_policy.json}"
 }
 
+# AWS IAM instance profile
+# TF: https://www.terraform.io/docs/providers/aws/r/iam_instance_profile.html
+# AWS: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/IAM_policies.html
 resource "aws_iam_instance_profile" "ecs_instance_profile" {
   name = "eric-instance-profile"
   role = "${aws_iam_role.ecs_role.name}"
 }
 
+# [Data] AWS ECS task definition
+# Simply specify the family to find the latest ACTIVE revision in that family.
+# TF: https://www.terraform.io/docs/providers/aws/d/ecs_task_definition.html
+# AWS: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definitions.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/ecs/register-task-definition.html
 data "aws_ecs_task_definition" "express_ecs_task_definition" {
   task_definition = "${aws_ecs_task_definition.express_ecs_task_definition.family}"
 }
 
+# AWS ECS task definition
+# TF: https://www.terraform.io/docs/providers/aws/d/ecs_task_definition.html
+# AWS: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definitions.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/ecs/register-task-definition.html
 resource "aws_ecs_task_definition" "express_ecs_task_definition" {
   family                = "eric-express"
   task_role_arn         = "${aws_iam_role.ecs_role.arn}"
@@ -74,6 +95,10 @@ resource "aws_ecs_task_definition" "express_ecs_task_definition" {
 DEFINITION
 }
 
+# AWS ECS service
+# TF: https://www.terraform.io/docs/providers/aws/r/ecs_service.html
+# AWS: http://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs_services.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/ecs/create-service.html
 resource "aws_ecs_service" "express_ecs_service" {
   name            = "eric-express-ecs-service"
   cluster         = "${aws_ecs_cluster.eric_express.id}"
@@ -81,6 +106,10 @@ resource "aws_ecs_service" "express_ecs_service" {
   task_definition = "${aws_ecs_task_definition.express_ecs_task_definition.family}:${max("${aws_ecs_task_definition.express_ecs_task_definition.revision}", "${data.aws_ecs_task_definition.express_ecs_task_definition.revision}")}"
 }
 
+# AWS EC2 instance
+# TF: https://www.terraform.io/docs/providers/aws/r/instance.html
+# AWS: http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/Instances.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/ec2/run-instances.html
 resource "aws_instance" "express_ec2" {
   ami                    = "${var.ec2_ami_id}"
   instance_type          = "${var.ec2_instance_type}"
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,7 @@
+# AWS S3 bucket
+# TF: https://www.terraform.io/docs/providers/aws/r/s3_bucket.html
+# AWS: http://docs.aws.amazon.com/AmazonS3/latest/dev/UsingBucket.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/s3api/create-bucket.html
 resource "aws_s3_bucket" "s3_data_bucket" {
   bucket = "${var.s3_data_bucket}"
   acl    = "private"
@@ -7,6 +11,10 @@ resource "aws_s3_bucket" "s3_data_bucket" {
   }
 }
 
+# [Data] IAM policy to define S3 permissions
+# TF: https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
+# AWS: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/iam/create-policy.html
 data "aws_iam_policy_document" "s3_data_bucket_policy" {
   statement {
     sid = ""
@@ -33,11 +41,19 @@ data "aws_iam_policy_document" "s3_data_bucket_policy" {
   }
 }
 
+# AWS IAM policy
+# TF: https://www.terraform.io/docs/providers/aws/r/iam_policy.html
+# AWS: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/iam/create-policy.html
 resource "aws_iam_policy" "s3_policy" {
   name   = "eric-s3-policy"
   policy = "${data.aws_iam_policy_document.s3_data_bucket_policy.json}"
 }
 
+# Attaches a managed IAM policy to an IAM role
+# TF: https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+# AWS: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_managed-vs-inline.html
+# AWS CLI: http://docs.aws.amazon.com/cli/latest/reference/iam/attach-role-policy.html
 resource "aws_iam_role_policy_attachment" "ecs_role_s3_data_bucket_policy_attach" {
   role       = "${aws_iam_role.ecs_role.name}"
   policy_arn = "${aws_iam_policy.s3_policy.arn}"
